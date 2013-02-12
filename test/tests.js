7e3457ec71e892b7ea8f84355373e19e2ad648ba
Joshua T Kalis <kalisjoshua@gmail.com>
2013-02-12 09:57:49 -0500

    refactoring test runner

--- test/tests.js
+++ test/tests.js
@@ -8,37 +8,29 @@ var assert  = require("chai").assert,
     easyXML = require("../index.js");
 
 describe("Node EasyXML", function () {
-  var tests = [
-    {
-      text: 'should parse a JSON object into XML',
-      id: ''
-    },
-    {
-      text: 'should parse a JSON object with attrs into XML',
-      id: '1'
-    },
-    {
-      text: 'should parse a JSON object with attrs and text node into XML',
-      id: '2'
-    }
-  ]
+  var should = {
+        "names"  : "should parse a JSON object into XML",
+        "names1" : "should parse a JSON object with attrs into XML",
+        "names2" : "should parse a JSON object with attrs and text node into XML"
+      };
 
-  tests.forEach(function(test){
-    it(test.text, function (done) {
-      var file = __dirname + "/fixtures/names"+test.id+".";
+  Object.keys(should)
+    .forEach(function(name){
+      it(should[name], function (done) {
+        var file = __dirname + "/fixtures/" + name;
 
-      fs.readFile(file + "xml", "UTF-8", function (err, data) {
-        if (err) {
-          throw err;
-        }
+        fs.readFile(file + ".xml", "UTF-8", function (err, data) {
+          if (err) {
+            throw err;
+          }
 
-        var json = require(file + "json");
+          var json = require(file + ".json");
 
-        assert.equal(easyXML.render(json), data, "EasyXML should create the correct XML from a JSON data structure.");
-        assert.strictEqual(easyXML.render(json), data, "EasyXML should create the correct XML from a JSON data structure.");
+          assert.equal(easyXML.render(json), data, "EasyXML should create the correct XML from a JSON data structure.");
+          assert.strictEqual(easyXML.render(json), data, "EasyXML should create the correct XML from a JSON data structure.");
 
-        done();
+          done();
+        });
       });
     });
-  });
 });
