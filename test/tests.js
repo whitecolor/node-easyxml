63887ab791b7ace1b54b1e3a98ec2f1a05701934
Dominik Weidenfeld <dom.weidenfeld@gmail.com>
2013-09-07 00:23:50 +0200

    fixing singularizeChildren functionality

--- test/tests.js
+++ test/tests.js
@@ -7,16 +7,33 @@ var assert  = require("chai").assert,
 
     easyXML = require("../index.js");
 
+var DEFAULT_OPTIONS = {
+            singularizeChildren: true,
+            underscoreAttributes: true,
+            rootElement: 'response',
+            dateFormat: 'ISO',
+            indent: 2,
+            manifest: true
+        };
+
 describe("Node EasyXML", function () {
   var should = {
         "names"  : "should parse a JSON object into XML",
         "names1" : "should parse a JSON object with attrs into XML",
-        "names2" : "should parse a JSON object with attrs and text node into XML"
+        "names2" : "should parse a JSON object with attrs and text node into XML",
+        "singularizeChildren" : "should parse a JSON object without singularizeChildren to XML",
+        "singularizeChildren2" : "should parse a JSON object without singularizeChildren to XML (with object)"
       };
 
   Object.keys(should)
     .forEach(function(name){
       it(should[name], function (done) {
+        if (name === 'singularizeChildren' || name === 'singularizeChildren2') {
+          easyXML.configure({ singularizeChildren: false });
+        } else {
+          easyXML.configure({ singularizeChildren: true });
+        }
+
         var file = __dirname + "/fixtures/" + name;
 
         fs.readFile(file + ".xml", "UTF-8", function (err, data) {
